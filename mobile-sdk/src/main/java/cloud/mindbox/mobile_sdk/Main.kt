c03e1c3bb6d6e3d92e26732b89bbeeae3a6e867d
Alexandra Batanova <alexandra.batanova@umbrellait.com>
2020-12-08 12:27:14 +0300

    add terporary code

---
+++ mobile-sdk/src/main/java/cloud/mindbox/mobile_sdk/Main.kt
@@ -0,0 +1,15 @@
+package cloud.mindbox.mobile_sdk
+
+
+/**
+ * Main class for test CI/CD
+ * @see createMessage() for functional
+ */
+class Main {
+
+    /**
+     * Main method for test CI/CD
+     * @return String test message
+     */
+    fun createMessage() = "Hello SDK"
+}
