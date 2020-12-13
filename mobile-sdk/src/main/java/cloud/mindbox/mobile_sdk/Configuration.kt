f629dbb92f454ac181c3de41f54f0de17dc63923
Alexandra Batanova <alexandra.batanova@umbrellait.com>
2020-12-13 15:01:58 +0300

    added get advertising info method

--- mobile-sdk/src/main/java/cloud/mindbox/mobile_sdk/Configuration.kt
+++ mobile-sdk/src/main/java/cloud/mindbox/mobile_sdk/Configuration.kt
@@ -1,5 +1,32 @@
 package cloud.mindbox.mobile_sdk
 
+import android.content.Context
+import android.util.Log
+import androidx.ads.identifier.AdvertisingIdClient
+
 class Configuration {
+
+    internal fun generateAdid(context: Context) {
+        if (AdvertisingIdClient.isAdvertisingIdProviderAvailable(context)) {
+            val advertisingIdInfoListenableFuture =
+                AdvertisingIdClient.getAdvertisingIdInfo(context)
+
+            advertisingIdInfoListenableFuture.addListener({
+                Log.d("Mindbox Debug", "Advertising load")
+                val adInfo = advertisingIdInfoListenableFuture.get()
+                val id: String? = adInfo?.id
+                val providerPackageName: String? = adInfo?.providerPackageName
+                val isLimitTrackingEnabled: Boolean? = adInfo?.isLimitAdTrackingEnabled
+                Log.d(
+                    "Mindbox Debug",
+                    "Advertising info: id - $id; providerPackageName - $providerPackageName; isLimitTrackingEnabled - $isLimitTrackingEnabled"
+                )
+            },
+                { command ->
+                    command?.run()
+                }
+            )
+        }
+    }
 
 }
