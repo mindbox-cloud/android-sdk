dd478146ad0c868d561410b9984c2102e18482a1
Alexandra Batanova <alexandra.batanova@umbrellait.com>
2020-12-21 16:03:19 +0300

    add layout for complete data scteen

---
+++ app/src/main/java/com/mindbox/androidsdk/screens/CompleteDataFragment.kt
@@ -0,0 +1,7 @@
+package com.mindbox.androidsdk.screens
+
+import androidx.fragment.app.Fragment
+import com.mindbox.androidsdk.R
+
+class CompleteDataFragment: Fragment(R.layout.fragment_entering_data) {
+}
