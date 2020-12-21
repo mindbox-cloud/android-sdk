c65705921c45778fb931d21429a9fc6f04d6301e
Alexandra Batanova <alexandra.batanova@umbrellait.com>
2020-12-21 16:13:39 +0300

    fill init data on complete screen

--- app/src/main/java/com/mindbox/androidsdk/screens/CompleteDataFragment.kt
+++ app/src/main/java/com/mindbox/androidsdk/screens/CompleteDataFragment.kt
@@ -1,7 +1,40 @@
 package com.mindbox.androidsdk.screens
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.mindbox.androidsdk.R
+import kotlinx.android.synthetic.main.fragment_complete_data.*
 
-class CompleteDataFragment: Fragment(R.layout.fragment_entering_data) {
+class CompleteDataFragment(private val endpoint: String, private val deviceId: String, private val installId: String) :
+    Fragment(R.layout.fragment_complete_data) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initParams.text = """
+            endpoint: $endpoint
+            
+            deviceUUID: $deviceId
+            
+            installId: $installId
+        """.trimIndent()
+
+
+    }
 }
+
+/**
+
+<!-- "Параметры инициализации SDK"
+
+- endpoint
+- deviceUUID
+- installationID
+
+"Данные из SDK API"
+
+- deviceUUID
+- token (дата получения)
+- версия SDK -->
+ */
