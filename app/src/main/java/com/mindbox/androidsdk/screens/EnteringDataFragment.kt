bcf37b45a6ad34adee5f03262101d09764699743
Alexandra Batanova <alexandra.batanova@umbrellait.com>
2020-12-21 09:06:37 +0300

    debaged parse responses

--- app/src/main/java/com/mindbox/androidsdk/screens/EnteringDataFragment.kt
+++ app/src/main/java/com/mindbox/androidsdk/screens/EnteringDataFragment.kt
@@ -5,9 +5,11 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import cloud.mindbox.mobile_sdk.Configuration
 import cloud.mindbox.mobile_sdk.Mindbox
+import cloud.mindbox.mobile_sdk.models.MindboxResponse
 import com.mindbox.androidsdk.Prefs
 import com.mindbox.androidsdk.R
 import kotlinx.android.synthetic.main.fragment_entering_data.*
+import org.json.JSONObject
 
 class EnteringDataFragment(callback: () -> Unit) :
     Fragment(R.layout.fragment_entering_data) {
@@ -36,6 +38,7 @@ class EnteringDataFragment(callback: () -> Unit) :
     }
 
     private fun initSdk() {
+        loadProgress.visibility = View.VISIBLE
         val endpoint = endpointData.text.toString()
         val deviceId = deviceUuidData.text.toString()
         val installId = installationIdData.text.toString()
@@ -44,6 +47,19 @@ class EnteringDataFragment(callback: () -> Unit) :
             Prefs.enteredEndpoint = endpoint
         }
 
-        Mindbox.registerSdk(this.requireContext(), endpoint, deviceId, installId)
+        Mindbox.registerSdk(this.requireContext(), endpoint, deviceId, installId) { response ->
+            activity?.runOnUiThread {
+                loadProgress.visibility = View.GONE
+                if (response is MindboxResponse.Error) {
+                    errorContainer.text =
+                        """code: ${response.status}
+                            |
+                            |message: ${response.message}
+                            |
+                            |error body: ${JSONObject(response.errorBody?.string())}
+                        """.trimMargin()
+                }
+            }
+        }
     }
 }
