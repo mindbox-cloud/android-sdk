0a4ad61be945a720db49d6dcd2e81a84c22d9c99
Alexandra Batanova <alexandra.batanova@umbrellait.com>
2021-01-25 22:46:31 +0300

    add db manager

---
+++ mobile-sdk/src/main/java/cloud/mindbox/mobile_sdk/managers/DbManager.kt
@@ -0,0 +1,24 @@
+package cloud.mindbox.mobile_sdk.managers
+
+import cloud.mindbox.mobile_sdk.models.Event
+import io.paperdb.Paper
+
+internal object DbManager {
+
+    private const val EVENTS_KEY = "events_stack"
+
+    fun addEventToStack(event: Event) {
+        //todo get all events
+        //todo rewrite it
+        //todo sync it
+        Paper.book().write(EVENTS_KEY, event)
+    }
+
+    fun getEventsStack() {
+
+    }
+
+    fun removeEventFromStack() {
+
+    }
+}
---
+++ mobile-sdk/src/main/java/cloud/mindbox/mobile_sdk/models/Event.kt
@@ -0,0 +1,5 @@
+package cloud.mindbox.mobile_sdk.models
+
+internal data class Event(
+    var id: String
+)
