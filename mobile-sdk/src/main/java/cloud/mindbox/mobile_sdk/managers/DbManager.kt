1d257808c35e2c3be6f229a87ca1e49723bd0f76
Alexandra Batanova <alexandra.batanova@umbrellait.com>
2021-01-26 01:39:23 +0300

    added methods for database manager

--- mobile-sdk/src/main/java/cloud/mindbox/mobile_sdk/managers/DbManager.kt
+++ mobile-sdk/src/main/java/cloud/mindbox/mobile_sdk/managers/DbManager.kt
@@ -1,33 +1,59 @@
 package cloud.mindbox.mobile_sdk.managers
 
 import cloud.mindbox.mobile_sdk.Configuration
+import cloud.mindbox.mobile_sdk.Logger
 import cloud.mindbox.mobile_sdk.models.Event
 import io.paperdb.Paper
+import io.paperdb.PaperDbException
 
+//todo sync it
+//todo save configs to hawk
 internal object DbManager {
 
-    private const val EVENTS_KEY = "events_stack"
+    private const val CONFIGURATION_KEY = "configuration_key"
+    private const val EVENTS_BOOK_NAME = "mindbox_events_book"
+    private const val CONFIGURATION_BOOK_NAME = "mindbox_configuration_book"
+
+    private val eventsBook = Paper.book(EVENTS_BOOK_NAME)
+    private val configurationBook = Paper.book(CONFIGURATION_BOOK_NAME)
 
     fun addEventToStack(event: Event) {
-        //todo get all events
-        //todo rewrite it
-        //todo sync it
-        Paper.book().write(EVENTS_KEY, event)
+        try {
+            eventsBook.write(event.transactionId, event)
+        } catch (exception: PaperDbException) {
+            Logger.e(this, "Error writing object ${event.transactionId} to the database", exception)
+        }
     }
 
-    fun getEventsStack() {
+    fun getEventsStack(): List<Event> {
+        val list = arrayListOf<Event>()
+        val keys = eventsBook.allKeys
 
+        for (key in keys) {
+            val value = getEvent(key) ?: continue
+            list.add(value)
+        }
+
+        return list
     }
 
-    fun removeEventFromStack() {
+    private fun getEvent(key: String): Event? {
+        return try {
+            eventsBook.read(key) as Event?
+        } catch (exception: PaperDbException) {
 
+            // invalid data in case of exception
+            removeEventFromStack(key)
+            Logger.e(this, "Error reading from database", exception)
+            null
+        }
     }
 
-    fun setConfiguration(configuration: Configuration) {
-
-    }
-
-    fun getConfiguration() {
-
+    fun removeEventFromStack(key: String) {
+        try {
+            eventsBook.delete(key)
+        } catch (exception: PaperDbException) {
+            Logger.e(this, "Error deleting item from database", exception)
+        }
     }
 }
