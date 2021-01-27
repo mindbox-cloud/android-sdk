1b7a646af001fe011df2714870e905315a5e3763
Alexandra Batanova <alexandra.batanova@umbrellait.com>
2021-01-27 12:17:03 +0300

    add synchronization

--- sdk/src/main/java/cloud/mindbox/mobile_sdk/managers/DbManager.kt
+++ sdk/src/main/java/cloud/mindbox/mobile_sdk/managers/DbManager.kt
@@ -7,8 +7,6 @@ import io.paperdb.Paper
 import io.paperdb.PaperDbException
 import java.util.*
 
-
-//todo sync it
 internal object DbManager {
 
     private const val CONFIGURATION_KEY = "configuration_key"
@@ -19,83 +17,101 @@ internal object DbManager {
     private val configurationBook = Paper.book(CONFIGURATION_BOOK_NAME)
 
     fun addEventToStack(event: Event) {
-        try {
-            eventsBook.write(event.transactionId, event)
-        } catch (exception: PaperDbException) {
-            Logger.e(this, "Error writing object ${event.transactionId} to the database", exception)
+        synchronized(this) {
+            try {
+                eventsBook.write(event.transactionId, event)
+            } catch (exception: PaperDbException) {
+                Logger.e(
+                    this,
+                    "Error writing object ${event.transactionId} to the database",
+                    exception
+                )
+            }
         }
     }
 
     fun getEventsStack(): List<Event> {
-        val list = arrayListOf<Event>()
-        val keys = eventsBook.allKeys
+        synchronized(this) {
+            val list = arrayListOf<Event>()
+            val keys = eventsBook.allKeys
 
-        for (key in keys) {
-            val value = getEvent(key) ?: continue
-            list.add(value)
+            for (key in keys) {
+                val value = getEvent(key) ?: continue
+                list.add(value)
+            }
+
+            return filterOldEvents(list)
         }
-
-        return filterOldEvents(list)
     }
 
     private fun getEvent(key: String): Event? {
-        return try {
-            eventsBook.read(key) as Event?
-        } catch (exception: PaperDbException) {
+        synchronized(this) {
+            return try {
+                eventsBook.read(key) as Event?
+            } catch (exception: PaperDbException) {
 
-            // invalid data in case of exception
-            removeEventFromStack(key)
-            Logger.e(this, "Error reading from database", exception)
-            null
+                // invalid data in case of exception
+                removeEventFromStack(key)
+                Logger.e(this, "Error reading from database", exception)
+                null
+            }
         }
     }
 
     fun removeEventFromStack(key: String) {
-        try {
-            eventsBook.delete(key)
-        } catch (exception: PaperDbException) {
-            Logger.e(this, "Error deleting item from database", exception)
+        synchronized(this) {
+            try {
+                eventsBook.delete(key)
+            } catch (exception: PaperDbException) {
+                Logger.e(this, "Error deleting item from database", exception)
+            }
         }
     }
 
     private fun filterOldEvents(list: ArrayList<Event>): List<Event> {
-        var filteredList = list.toList()
+        synchronized(this) {
+            var filteredList = list.toList()
 
-        // filter by volume
-        if (list.size > 10000) {
-            for (i in 1..(list.size - 10000)) {
-                removeEventFromStack(list[i].transactionId)
+            // filter by volume
+            if (list.size > 10000) {
+                for (i in 1..(list.size - 10000)) {
+                    removeEventFromStack(list[i].transactionId)
+                }
+                filteredList = list.filterIndexed { index, _ -> index > (list.size - 10000) }
             }
-            filteredList = list.filterIndexed { index, _ -> index > (list.size - 10000) }
-        }
 
-        //todo
-        // filter by time
+            //todo
+            // filter by time
 //        val now = Date().time
 //
 //        filteredList.forEach { event ->
 //            val date: LocalDate = ofEpochMilli(event.enqueueTimestamp).atZone(ZoneId.systemDefault()).toLocalDate()
 //            if (event.enqueueTimestamp / 1000 * 60 * 60 ==)
 //        }
-        return filteredList
+            return filteredList
+        }
     }
 
     fun saveConfigurations(configuration: Configuration) {
-        try {
-            configurationBook.write(CONFIGURATION_KEY, configuration)
-        } catch (exception: PaperDbException) {
-            Logger.e(this, "Error writing object configuration to the database", exception)
+        synchronized(this) {
+            try {
+                configurationBook.write(CONFIGURATION_KEY, configuration)
+            } catch (exception: PaperDbException) {
+                Logger.e(this, "Error writing object configuration to the database", exception)
+            }
         }
     }
 
     fun getConfigurations(): Configuration? {
-        return try {
-            configurationBook.read(CONFIGURATION_KEY) as Configuration?
-        } catch (exception: PaperDbException) {
+        synchronized(this) {
+            return try {
+                configurationBook.read(CONFIGURATION_KEY) as Configuration?
+            } catch (exception: PaperDbException) {
 
-            // invalid data in case of exception
-            Logger.e(this, "Error reading from database", exception)
-            null
+                // invalid data in case of exception
+                Logger.e(this, "Error reading from database", exception)
+                null
+            }
         }
     }
 }
