5e685352683e0d8a43f14c2b2c5f7d39e5495757
Alexandra Batanova <alexandra.batanova@umbrellait.com>
2021-02-12 15:47:33 +0300

    update version to 1.0.15

--- app/src/main/java/com/mindbox/androidsdk/PushokMessagingService.kt
+++ app/src/main/java/com/mindbox/androidsdk/PushokMessagingService.kt
@@ -1,5 +1,9 @@
 package com.mindbox.androidsdk
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
@@ -38,11 +42,30 @@ class PushokMessagingService : FirebaseMessagingService() {
             .setContentText(description)
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
 
+        createNotificationChannel()
+
         with(NotificationManagerCompat.from(this)) {
             // notificationId is a unique int for each notification that you must define
             notify(Random.nextInt(0, 100), builder.build())
         }
 
         super.onMessageReceived(remoteMessage)
+    }
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.pushok_default_channel)
+            val descriptionText = getString(R.string.pushok_default_channel)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(getString(R.string.pushok_default_channel), name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 }
