77b213f6fd4a95eeedde4f2aa4f0fceda64c1a32
Alexandra Batanova <alexandra.batanova@umbrellait.com>
2021-03-01 14:08:26 +0300

    changes with type-safety

--- sdk/src/main/java/cloud/mindbox/mobile_sdk/services/MindboxMessagingService.kt
+++ sdk/src/main/java/cloud/mindbox/mobile_sdk/services/MindboxMessagingService.kt
@@ -14,16 +14,19 @@ class MindboxMessagingService : FirebaseMessagingService() {
         if (token.isNotEmpty() && token != MindboxPreferences.firebaseToken) {
             Paper.init(applicationContext)
 
-            if (DbManager.getConfigurations() == null) {
+            val configurations = DbManager.getConfigurations()
+
+            if (configurations == null) {
                 Logger.w(this, "Received FMS token, but SDK not initialized")
                 return
             }
 
-            Mindbox.init(applicationContext, DbManager.getConfigurations()!!)
+            Mindbox.init(applicationContext, configurations)
         }
     }
 
     override fun onMessageReceived(message: RemoteMessage) {
+        //todo add parse message and getting uniqKey
         Mindbox.onPushReceived(applicationContext, "")
     }
 }
