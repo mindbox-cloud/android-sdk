887e0629a62b95d886d461102e85d332d572bc37
Roman Parfinenko <roman.parfinenko@umbrella-web.com>
2021-03-18 18:35:19 +0300

    git username replaced with secret variable

--- git-release.sh
+++ git-release.sh
@@ -3,9 +3,9 @@ set -e
 version=$(cat gradle.properties |grep SDK_VERSION_NAME | cut -f2 -d"=")
 text=$1
 token=$2
+user=$3
 branch=$(git rev-parse --abbrev-ref HEAD)
 repo_full_name=$(git config --get remote.origin.url | sed 's/.*:\/\/github.com\///;s/.git$//' | cut -f2 -d":")
-git_org=
 generate_post_data()
 {
   cat <<EOF
@@ -20,4 +20,5 @@ generate_post_data()
 EOF
 }
 echo "Create release $version for repo: $repo_full_name branch: $branch"
-curl --user "JitPack-IT:$token" --data "$(generate_post_data)" "https://api.github.com/repos/$repo_full_name/releases"
+echo "Release settings: $(generate_post_data)"
+curl -s --show-error --user "$user:$token" --data "$(generate_post_data)" "https://api.github.com/repos/$repo_full_name/releases"
