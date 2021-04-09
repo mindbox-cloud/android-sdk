9702cb51b92b8f4dd75d171252af3de262f7ab71
Alexandra Batanova <alexandra.batanova@umbrellait.com>
2021-04-09 14:59:03 +0300

    start work with unit-tests

---
+++ sdk/src/androidTest/java/cloud/mindbox/mobile_sdk/VersioningUnitTest.kt
@@ -0,0 +1,31 @@
+package cloud.mindbox.mobile_sdk
+
+import org.junit.AfterClass
+import org.junit.BeforeClass
+import org.junit.Test
+
+class VersioningUnitTest {
+
+    companion object {
+        @BeforeClass
+        @JvmStatic
+        fun generateData() {
+
+            for (i in 0..50) {
+                Mindbox.updateFmsToken()
+            }
+        }
+
+        @AfterClass
+        @JvmStatic
+        fun clearData() {
+
+        }
+    }
+
+    @Test
+    fun generatedData_isCorrect() {
+
+    }
+
+}
