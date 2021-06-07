036f64554bb2bf3f6c9c31db1a128eb540484550
Elizaveta Nevostrueva <elizaveta.nevostrueva@umbrellait.com>
2021-06-07 15:35:33 +0300

    Improved classes

--- sdk/src/main/java/cloud/mindbox/mobile_sdk/models/operation/request/RecommendationRequest.kt
+++ sdk/src/main/java/cloud/mindbox/mobile_sdk/models/operation/request/RecommendationRequest.kt
@@ -5,5 +5,6 @@ import com.google.gson.annotations.SerializedName
 open class RecommendationRequest(
     @SerializedName("limit") val limit: Int? = null,
     @SerializedName("area") val area: AreaRequest? = null,
-    @SerializedName("productCategory") val productCategory: ProductCategoryRequest? = null
+    @SerializedName("productCategory") val productCategory: ProductCategoryRequest? = null,
+    @SerializedName("product") val product: ProductRequest? = null
 )
--- sdk/src/main/java/cloud/mindbox/mobile_sdk/models/operation/response/OperationResponse.kt
+++ sdk/src/main/java/cloud/mindbox/mobile_sdk/models/operation/response/OperationResponse.kt
@@ -1,15 +1,23 @@
 package cloud.mindbox.mobile_sdk.models.operation.response
 
+import cloud.mindbox.mobile_sdk.models.operation.request.CatalogProductListRequest
+import cloud.mindbox.mobile_sdk.models.operation.request.ProductListItemRequest
 import com.google.gson.annotations.SerializedName
 
 open class OperationResponse(
     status: String? = null,
     @SerializedName("customer") val customer: CustomerResponse? = null,
-    @SerializedName("productList") val productList: List<ProductListItemResponse>? = null,
+    @SerializedName("productList") val productList: Any? = null,
     @SerializedName("recommendations") val recommendations: List<RecommendationResponse>? = null,
     @SerializedName("customerSegmentations") val customerSegmentations: List<CustomerSegmentationResponse>? = null,
     @SerializedName("promoCode") val promoCode: PromoCodeResponse? = null
-) : OperationResponseBase(status){
+) : OperationResponseBase(status) {
+
+    /** Used for catalog with name productList and its type is [CatalogProductListRequest] **/
+    fun productList(): CatalogProductListRequest? = productList as? CatalogProductListRequest
+
+    /** Used for product with name productList and its is array of [ProductListItemRequest] **/
+    fun productListItems() = (productList as? List<*>)?.mapNotNull { it as? ProductListItemRequest }
 
     override fun toString(): String {
         return "OperationResponse(customer=$customer, productList=$productList, recommendations=$recommendations, customerSegmentations=$customerSegmentations, promoCode=$promoCode)"
