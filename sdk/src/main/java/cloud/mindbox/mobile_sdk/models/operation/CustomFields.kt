4604df55797f519ec721e5384226513f4a481d24
Elizaveta Nevostrueva <elizaveta.nevostrueva@umbrellait.com>
2021-06-16 22:49:34 +0300

    Added constructors

--- sdk/src/main/java/cloud/mindbox/mobile_sdk/models/operation/CustomFields.kt
+++ sdk/src/main/java/cloud/mindbox/mobile_sdk/models/operation/CustomFields.kt
@@ -4,6 +4,7 @@ import cloud.mindbox.mobile_sdk.models.operation.adapters.CustomerFieldsAdapter
 import cloud.mindbox.mobile_sdk.returnOnException
 import com.google.gson.Gson
 import com.google.gson.annotations.JsonAdapter
+import androidx.core.util.Pair as AndroidXPair
 
 @JsonAdapter(CustomerFieldsAdapter::class)
 class CustomFields(val fields: Map<String, Any?>? = null) {
@@ -19,6 +20,11 @@ class CustomFields(val fields: Map<String, Any?>? = null) {
     }.returnOnException { null }
 
     constructor(vararg pairs: Pair<String, Any?>) : this(pairs.toMap())
+
+    constructor(
+        vararg pairs: AndroidXPair<String, Any?>
+    ) : this(*pairs.mapNotNull { pair -> pair.first?.let { Pair(it, pair.second) } }.toTypedArray())
+
 
     override fun toString() = "CustomFields(fields=$fields)"
 
--- sdk/src/main/java/cloud/mindbox/mobile_sdk/models/operation/Ids.kt
+++ sdk/src/main/java/cloud/mindbox/mobile_sdk/models/operation/Ids.kt
@@ -2,6 +2,7 @@ package cloud.mindbox.mobile_sdk.models.operation
 
 import cloud.mindbox.mobile_sdk.models.operation.adapters.IdsAdapter
 import com.google.gson.annotations.JsonAdapter
+import androidx.core.util.Pair as AndroidXPair
 
 @JsonAdapter(IdsAdapter::class)
 class Ids(val ids: Map<String, String?>) {
@@ -15,6 +16,10 @@ class Ids(val ids: Map<String, String?>) {
     constructor(vararg pairs: Pair<String, String?>) : this(pairs.toMap())
 
     constructor(
+        vararg pairs: AndroidXPair<String, String?>
+    ) : this(*pairs.mapNotNull { pair -> pair.first?.let { Pair(it, pair.second) } }.toTypedArray())
+
+    constructor(
         mindboxId: Int,
         ids: Map<String, String?>
     ) : this(HashMap<String, String?>(ids).apply { this[MINDBOX_ID_KEY] = "$mindboxId" })
@@ -24,6 +29,18 @@ class Ids(val ids: Map<String, String?>) {
         vararg pairs: Pair<String, String?>
     ) : this(HashMap<String, String?>(pairs.toMap()).apply { this[MINDBOX_ID_KEY] = "$mindboxId" })
 
+    constructor(
+        mindboxId: Int,
+        vararg pairs: AndroidXPair<String, String?>
+    ) : this(
+        mindboxId = mindboxId,
+        *pairs.mapNotNull { pair -> pair.first?.let { Pair(it, pair.second) } }.toTypedArray()
+    )
+
     override fun toString() = "Ids(ids=$ids)"
+
+    private fun convertToKotlinPair(
+        pair: AndroidXPair<String, String?>
+    ) = pair.first?.let { Pair(it, pair.second) }
 
 }
