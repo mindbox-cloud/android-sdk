48c47405a1c267ede918f0f01374d289545c42e0
artem.petrov <artem.petrov@umbrellait.com>
2021-07-02 19:28:16 +0300

    Add toString in TypeResponse

--- sdk/src/main/java/cloud/mindbox/mobile_sdk/models/operation/response/TypeResponse.kt
+++ sdk/src/main/java/cloud/mindbox/mobile_sdk/models/operation/response/TypeResponse.kt
@@ -6,4 +6,8 @@ import com.google.gson.annotations.SerializedName
 open class TypeResponse(
     @SerializedName("ids") val ids: Ids? = null,
     @SerializedName("name") val name: String? = null
-)
+) {
+
+    override fun toString() = "TypeResponse(ids=$ids, name=$name)"
+
+}
