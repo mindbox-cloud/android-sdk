04ed18e0f8c02050c46ab90dfb2dcb95d9415598
artem.petrov <artem.petrov@umbrellait.com>
2021-07-02 19:28:25 +0300

    Fix style

--- sdk/src/main/java/cloud/mindbox/mobile_sdk/models/operation/response/PeriodType.kt
+++ sdk/src/main/java/cloud/mindbox/mobile_sdk/models/operation/response/PeriodType.kt
@@ -3,7 +3,9 @@ package cloud.mindbox.mobile_sdk.models.operation.response
 import com.google.gson.annotations.SerializedName
 
 enum class PeriodType {
+
     @SerializedName("FixedDays") FIXED_DAYS,
     @SerializedName("FixedWeeks") FIXED_WEEKS,
     @SerializedName("FixedMonths") FIXED_MONTHS
+
 }
