904a5e7394950c3c903a943cbecb41c38f5cce3d
artem.petrov <artem.petrov@umbrellait.com>
2021-07-08 16:04:26 +0300

    Add price field to ProductListItemResponse

--- sdk/src/main/java/cloud/mindbox/mobile_sdk/models/operation/response/ProductListItemResponse.kt
+++ sdk/src/main/java/cloud/mindbox/mobile_sdk/models/operation/response/ProductListItemResponse.kt
@@ -7,7 +7,8 @@ open class ProductListItemResponse constructor(
     @SerializedName("product") val product: ProductResponse? = null,
     @SerializedName("productGroup") val productGroup: ProductGroupResponse? = null,
     @SerializedName("pricePerItem") val pricePerItem: Double? = null,
-    @SerializedName("priceOfLine") val priceOfLine: Double? = null
+    @SerializedName("priceOfLine") val priceOfLine: Double? = null,
+    @SerializedName("price") val price: Double? = null
 ) {
 
     override fun toString() = "ProductListItemResponse(count=$count, product=$product, " +
