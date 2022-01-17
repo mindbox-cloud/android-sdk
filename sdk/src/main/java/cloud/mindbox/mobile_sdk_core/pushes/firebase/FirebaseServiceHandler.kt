bcf6a8957752e26058ad10750cc0b731d53cba42
artem.petrov <artem.petrov@umbrellait.com>
2022-01-17 21:15:29 +0300

    Merge architecture-rework into 205_notification_provider_field

--- sdk/src/main/java/cloud/mindbox/mobile_sdk_core/pushes/firebase/FirebaseServiceHandler.kt
+++ sdk/src/main/java/cloud/mindbox/mobile_sdk_core/pushes/firebase/FirebaseServiceHandler.kt
@@ -24,8 +24,6 @@ import java.util.concurrent.TimeUnit
 
 object FirebaseServiceHandler : PushServiceHandler() {
 
-    override val notificationProvider: String = "STUB"
-
     private const val ZERO_ID = "00000000-0000-0000-0000-000000000000"
 
     override val notificationProvider: String = "FCM"
