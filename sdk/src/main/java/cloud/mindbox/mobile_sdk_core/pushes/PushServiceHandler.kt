67ebca277c6f6cb266e0ccddf1a90b53e9caae1c
artem.petrov <artem.petrov@umbrellait.com>
2022-01-27 09:41:13 +0300

    Fix style

--- sdk/src/main/java/cloud/mindbox/mobile_sdk_core/pushes/PushServiceHandler.kt
+++ sdk/src/main/java/cloud/mindbox/mobile_sdk_core/pushes/PushServiceHandler.kt
@@ -9,7 +9,9 @@ import java.util.*
 abstract class PushServiceHandler {
 
     companion object {
+
         private const val ZERO_ID = "00000000-0000-0000-0000-000000000000"
+
     }
 
     abstract val notificationProvider: String
