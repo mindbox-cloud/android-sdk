5668a256d94bf9cc47afdc0aaffba2e64022de6b
artem.petrov <artem.petrov@umbrellait.com>
2022-03-05 17:10:19 +0300

    Create WorkerFactory

---
+++ sdk/src/main/java/cloud/mindbox/mobile_sdk/MindboxWorkerFactory.kt
@@ -0,0 +1,19 @@
+package cloud.mindbox.mobile_sdk
+
+import android.content.Context
+import androidx.work.ListenableWorker
+import androidx.work.WorkerFactory
+import androidx.work.WorkerParameters
+import cloud.mindbox.mobile_sdk.services.MindboxOneTimeEventWorker
+
+object MindboxWorkerFactory : WorkerFactory() {
+
+    override fun createWorker(
+        appContext: Context,
+        workerClassName: String,
+        workerParameters: WorkerParameters,
+    ): ListenableWorker? = workerClassName
+        .takeIf { it == MindboxOneTimeEventWorker::class.qualifiedName }
+        ?.let { MindboxOneTimeEventWorker(appContext, workerParameters) }
+
+}
