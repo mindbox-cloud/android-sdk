4e4084ae6bc75c46e53f25d9b01118d7c6865670
artem.petrov <artem.petrov@umbrellait.com>
2022-04-08 10:42:31 +0300

    Make pushServices obligatory

--- sdk/src/main/java/cloud/mindbox/mobile_sdk/Mindbox.kt
+++ sdk/src/main/java/cloud/mindbox/mobile_sdk/Mindbox.kt
@@ -280,7 +280,7 @@ object Mindbox {
     fun init(
         context: Context,
         configuration: MindboxConfiguration,
-        pushServices: List<MindboxPushService>? = null,
+        pushServices: List<MindboxPushService>?,
     ) {
         LoggingExceptionHandler.runCatching {
             initComponents(context, pushServices)
