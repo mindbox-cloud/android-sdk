9495148714b2767e92834fd61cd00402a72c81a7
Robert Alimbekov <robert.alimbekov@rentateam.ru>
2023-02-01 17:17:32 +0500

    fixing tests

--- sdk/src/test/java/cloud/mindbox/mobile_sdk/monitoring/LogResponseStub.kt
+++ sdk/src/test/java/cloud/mindbox/mobile_sdk/monitoring/LogResponseStub.kt
@@ -1,9 +1,10 @@
 package cloud.mindbox.mobile_sdk.monitoring
 
 import cloud.mindbox.mobile_sdk.monitoring.domain.models.LogResponse
+import java.time.ZonedDateTime
 
 internal class LogResponseStub {
     companion object {
-        fun get() = LogResponse(zonedDateTime = "", log = "")
+        fun get() = LogResponse(zonedDateTime = ZonedDateTime.now(), log = "")
     }
 }
