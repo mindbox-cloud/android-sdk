6ee4429d69237c9a7c5d4b5e9d09289f4a633535
Robert Alimbekov <robert.alimbekov@rentateam.ru>
2023-03-01 19:50:13 +0500

    clearing table

--- sdk/src/main/java/cloud/mindbox/mobile_sdk/monitoring/data/room/MonitoringDatabase.kt
+++ sdk/src/main/java/cloud/mindbox/mobile_sdk/monitoring/data/room/MonitoringDatabase.kt
@@ -2,11 +2,22 @@ package cloud.mindbox.mobile_sdk.monitoring.data.room
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import cloud.mindbox.mobile_sdk.monitoring.data.room.dao.MonitoringDao
 import cloud.mindbox.mobile_sdk.monitoring.data.room.entities.MonitoringEntity
 
-@Database(entities = [MonitoringEntity::class], version = 1)
+@Database(entities = [MonitoringEntity::class], version = 2)
 internal abstract class MonitoringDatabase : RoomDatabase() {
+
+    companion object {
+        val MIGRATION_1_2 = object: Migration(1, 2) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL("DELETE FROM monitoring")
+                database.execSQL("ALTER TABLE monitoring RENAME TO mb_monitoring")
+            }
+        }
+    }
 
     abstract fun monitoringDao(): MonitoringDao
 
--- sdk/src/main/java/cloud/mindbox/mobile_sdk/monitoring/data/room/dao/MonitoringDao.kt
+++ sdk/src/main/java/cloud/mindbox/mobile_sdk/monitoring/data/room/dao/MonitoringDao.kt
@@ -10,21 +10,21 @@ internal interface MonitoringDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertLog(entity: MonitoringEntity)
 
-    @Query("DELETE FROM monitoring WHERE id IN (SELECT id FROM monitoring ORDER BY id ASC LIMIT (SELECT CNT/10 FROM (SELECT COUNT(id) as CNT FROM monitoring)))")
+    @Query("DELETE FROM ${MonitoringEntity.TABLE_NAME} WHERE id IN (SELECT id FROM ${MonitoringEntity.TABLE_NAME} ORDER BY id ASC LIMIT (SELECT CNT/10 FROM (SELECT COUNT(id) as CNT FROM ${MonitoringEntity.TABLE_NAME})))")
     suspend fun deleteFirstTenPercentOfLogs()
 
     @Delete
     suspend fun deleteLog(log: MonitoringEntity)
 
-    @Query("DELETE FROM monitoring WHERE id IN (SELECT id FROM monitoring ORDER BY id ASC LIMIT 1)")
+    @Query("DELETE FROM ${MonitoringEntity.TABLE_NAME} WHERE id IN (SELECT id FROM ${MonitoringEntity.TABLE_NAME} ORDER BY id ASC LIMIT 1)")
     suspend fun deleteFirstLog()
 
-    @Query("SELECT * FROM monitoring WHERE timestamp BETWEEN :startTime and :endTime ORDER BY timestamp ASC")
+    @Query("SELECT * FROM ${MonitoringEntity.TABLE_NAME} WHERE timestamp BETWEEN :startTime and :endTime ORDER BY timestamp ASC")
     suspend fun getLogs(startTime: String, endTime: String): List<MonitoringEntity>
 
-    @Query("SELECT * FROM monitoring ORDER BY id ASC LIMIT 1")
+    @Query("SELECT * FROM ${MonitoringEntity.TABLE_NAME} ORDER BY id ASC LIMIT 1")
     suspend fun getFirstLog(): MonitoringEntity
 
-    @Query("SELECT * FROM monitoring ORDER BY id DESC LIMIT 1")
+    @Query("SELECT * FROM ${MonitoringEntity.TABLE_NAME} ORDER BY id DESC LIMIT 1")
     suspend fun getLastLog(): MonitoringEntity
 }
