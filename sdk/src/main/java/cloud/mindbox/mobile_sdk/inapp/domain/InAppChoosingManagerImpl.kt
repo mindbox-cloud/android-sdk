8dd541311839d12df994e6c8f5e6389ada89bbc3
Robert Alimbekov <robert.alimbekov@rentateam.ru>
2023-04-10 16:15:45 +0500

    bug fix

--- sdk/src/main/java/cloud/mindbox/mobile_sdk/inapp/domain/InAppChoosingManagerImpl.kt
+++ sdk/src/main/java/cloud/mindbox/mobile_sdk/inapp/domain/InAppChoosingManagerImpl.kt
@@ -1,7 +1,6 @@
 package cloud.mindbox.mobile_sdk.inapp.domain
 
 import cloud.mindbox.mobile_sdk.inapp.domain.interfaces.managers.InAppChoosingManager
-import cloud.mindbox.mobile_sdk.inapp.domain.interfaces.managers.InAppFilteringManager
 import cloud.mindbox.mobile_sdk.inapp.domain.interfaces.repositories.InAppGeoRepository
 import cloud.mindbox.mobile_sdk.inapp.domain.interfaces.repositories.InAppSegmentationRepository
 import cloud.mindbox.mobile_sdk.inapp.domain.models.*
@@ -11,7 +10,7 @@ import cloud.mindbox.mobile_sdk.models.InAppEventType
 
 internal class InAppChoosingManagerImpl(
     private val inAppGeoRepository: InAppGeoRepository,
-    private val inAppSegmentationRepository: InAppSegmentationRepository
+    private val inAppSegmentationRepository: InAppSegmentationRepository,
 ) :
     InAppChoosingManager {
 
@@ -24,18 +23,18 @@ internal class InAppChoosingManagerImpl(
             runCatching {
                 inApp.targeting.fetchTargetingInfo(data)
             }.onFailure { throwable ->
-                when (throwable) {
+               return when (throwable) {
                     is GeoError -> {
                         inAppGeoRepository.setGeoStatus(GeoFetchStatus.GEO_FETCH_ERROR)
                         MindboxLoggerImpl.e(this, "Error fetching geo", throwable)
-                        inApp.targeting.fetchTargetingInfo(getTargetingData(triggerEvent))
+                        chooseInAppToShow(inApps, triggerEvent)
                     }
                     is SegmentationError -> {
                         inAppSegmentationRepository.setCustomerSegmentationStatus(
                             SegmentationFetchStatus.SEGMENTATION_FETCH_ERROR
                         )
                         MindboxLoggerImpl.e(this, "Error fetching segmentations", throwable)
-                        inApp.targeting.fetchTargetingInfo(getTargetingData(triggerEvent))
+                        chooseInAppToShow(inApps, triggerEvent)
                     }
                     else -> {
                         MindboxLoggerImpl.e(this, throwable.message ?: "", throwable)
