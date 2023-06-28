54f9dc04148b6c3f96db30898764f19e2254df28
robalim <robert.alimbekov@masterforminecraft.com>
2023-06-28 11:58:17 +0300

    test workflow

--- sdk/src/main/java/cloud/mindbox/mobile_sdk/Mindbox.kt
+++ sdk/src/main/java/cloud/mindbox/mobile_sdk/Mindbox.kt
@@ -388,7 +388,7 @@ object Mindbox: MindboxLog {
                 logW("Skip Mindbox init not in main process! Current process $currentProcessName")
                 return@runCatching
             }
-            logI("Text to test the correct version of sdk5")
+            logI("Text to test the correct version of sdk6")
             initComponents(context.applicationContext, pushServices)
             logI("init in $currentProcessName. firstInitCall: $firstInitCall, " +
                     "configuration: $configuration, pushServices: " +
