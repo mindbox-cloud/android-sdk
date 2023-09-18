7d612a2b1ab45aeb547d4cf0d72de0202421c4d7
Robert Alimbekov <robert.alimbekov@rentateam.ru>
2023-09-19 01:32:57 +0500

    adding validation

--- sdk/src/main/java/cloud/mindbox/mobile_sdk/inapp/data/validators/ImageLayerValidator.kt
+++ sdk/src/main/java/cloud/mindbox/mobile_sdk/inapp/data/validators/ImageLayerValidator.kt
@@ -1,26 +1,27 @@
 package cloud.mindbox.mobile_sdk.inapp.data.validators
 
-import cloud.mindbox.mobile_sdk.inapp.data.dto.BackgroundDto
+import cloud.mindbox.mobile_sdk.inapp.data.dto.BackgroundDto.*
+import cloud.mindbox.mobile_sdk.inapp.data.dto.BackgroundDto.LayerDto.*
 
-internal class ImageLayerValidator : Validator<BackgroundDto.LayerDto.ImageLayerDto?> {
+internal class ImageLayerValidator : Validator<ImageLayerDto?> {
 
     private val actionValidator: ActionValidator =
         ActionValidator()
     private val sourceValidator: SourceValidator =
         SourceValidator()
 
-    override fun isValid(item: BackgroundDto.LayerDto.ImageLayerDto?): Boolean {
-        return item?.type == BackgroundDto.LayerDto.ImageLayerDto.IMAGE_TYPE_JSON_NAME &&
+    override fun isValid(item: ImageLayerDto?): Boolean {
+        return item?.type == ImageLayerDto.IMAGE_TYPE_JSON_NAME &&
                 actionValidator.isValid(item.action) &&
                 sourceValidator.isValid(item.source)
     }
 
     internal class SourceValidator :
-        Validator<BackgroundDto.LayerDto.ImageLayerDto.SourceDto?> {
-        override fun isValid(item: BackgroundDto.LayerDto.ImageLayerDto.SourceDto?): Boolean {
+        Validator<ImageLayerDto.SourceDto?> {
+        override fun isValid(item: ImageLayerDto.SourceDto?): Boolean {
             return when (item) {
-                is BackgroundDto.LayerDto.ImageLayerDto.SourceDto.UrlSourceDto -> {
-                    item.type == BackgroundDto.LayerDto.ImageLayerDto.SourceDto.UrlSourceDto.URL_SOURCE_JSON_NAME
+                is ImageLayerDto.SourceDto.UrlSourceDto -> {
+                    item.type == ImageLayerDto.SourceDto.UrlSourceDto.URL_SOURCE_JSON_NAME
                             && item.value != null
                 }
 
@@ -32,11 +33,11 @@ internal class ImageLayerValidator : Validator<BackgroundDto.LayerDto.ImageLayer
     }
 
     internal class ActionValidator :
-        Validator<BackgroundDto.LayerDto.ImageLayerDto.ActionDto?> {
-        override fun isValid(item: BackgroundDto.LayerDto.ImageLayerDto.ActionDto?): Boolean {
+        Validator<ImageLayerDto.ActionDto?> {
+        override fun isValid(item: ImageLayerDto.ActionDto?): Boolean {
             return when {
-                (item is BackgroundDto.LayerDto.ImageLayerDto.ActionDto.RedirectUrlActionDto) -> {
-                    item.type == BackgroundDto.LayerDto.ImageLayerDto.ActionDto.RedirectUrlActionDto.REDIRECT_URL_ACTION_TYPE_JSON_NAME
+                (item is ImageLayerDto.ActionDto.RedirectUrlActionDto) -> {
+                    item.type == ImageLayerDto.ActionDto.RedirectUrlActionDto.REDIRECT_URL_ACTION_TYPE_JSON_NAME
                             && item.value != null && item.intentPayload != null
                 }
                 else -> {
