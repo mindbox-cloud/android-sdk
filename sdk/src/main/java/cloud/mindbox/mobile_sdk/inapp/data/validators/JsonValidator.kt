a9d1edc576e2e4533f116831161983183c2a75a9
Sergey Sozinov <sergsozinov@gmail.com>
2024-01-09 16:57:23 +0300

    Changed JsonValidator

--- sdk/src/main/java/cloud/mindbox/mobile_sdk/inapp/data/validators/JsonValidator.kt
+++ sdk/src/main/java/cloud/mindbox/mobile_sdk/inapp/data/validators/JsonValidator.kt
@@ -1,11 +1,14 @@
 package cloud.mindbox.mobile_sdk.inapp.data.validators
 
+import com.google.gson.JsonParser
 
 internal class JsonValidator : Validator<String?> {
 
     override fun isValid(item: String?): Boolean {
         if (item.isNullOrBlank()) return false
-        val jsonRegex = "^\\s*(\\{.*\\}|\\[.*])\\s*$"
-        return item.trim().matches(jsonRegex.toRegex())
+
+        return runCatching {
+            JsonParser.parseString(item).let { it.isJsonObject || it.isJsonArray }
+        }.getOrElse { false }
     }
 }
