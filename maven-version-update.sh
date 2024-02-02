dd742db62324b6be862cba98c64f1e00ff2ab578
Robert Alimbekov <robert.alimbekov@rentateam.ru>
2024-02-02 16:01:10 +0500

    adding readme

--- maven-version-update.sh
+++ maven-version-update.sh
@@ -5,5 +5,4 @@ function getProperty {
     PROP_VALUE=`cat $GRADLE_PROPERTIES_FILE | grep -w "$PROP_KEY" | cut -d'=' -f2`
     echo $PROP_VALUE
 }
-SDK_VERSION_NAME=$(getProperty "SDK_VERSION_NAME")
 sed -i -e "s|https://search.maven.org/artifact/cloud.mindbox/mobile-sdk/[0-9].[0-9].[0-9]/aar|https://search.maven.org/artifact/cloud.mindbox/mobile-sdk/${SDK_VERSION_NAME}/aar|g" README.md
