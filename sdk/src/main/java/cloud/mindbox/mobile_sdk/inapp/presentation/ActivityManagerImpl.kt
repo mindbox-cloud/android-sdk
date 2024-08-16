69fe121ed8f206e1abc4a469f4fdd1c9c11e5259
Sergey Sozinov <sozinov@mindbox.cloud>
2024-08-16 15:44:13 +0300

    MBX-3577: updated tryOpenDeepLinkMethod

--- sdk/src/main/java/cloud/mindbox/mobile_sdk/inapp/presentation/ActivityManagerImpl.kt
+++ sdk/src/main/java/cloud/mindbox/mobile_sdk/inapp/presentation/ActivityManagerImpl.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import cloud.mindbox.mobile_sdk.inapp.domain.interfaces.interactors.CallbackInteractor
-import cloud.mindbox.mobile_sdk.logger.mindboxLogE
+import cloud.mindbox.mobile_sdk.utils.LoggingExceptionHandler
 
 
 internal class ActivityManagerImpl(
@@ -12,6 +12,9 @@ internal class ActivityManagerImpl(
     private val context: Context
 ) : ActivityManager {
 
+    companion object {
+        private const val ALL_MATCHING_ACTIVITIES_FLAG: Int = 0
+    }
 
     override fun tryOpenUrl(url: String): Boolean {
         try {
@@ -27,22 +30,28 @@ internal class ActivityManagerImpl(
         }
     }
 
-    override fun tryOpenDeepLink(deepLink: String): Boolean {
-        try {
-            Intent(Intent.ACTION_VIEW, Uri.parse(deepLink)).also { intent ->
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                return if (intent.resolveActivity(context.packageManager)!= null) {
-                    if (intent.resolveActivityInfo(context.packageManager, 0)?.exported == false) {
+    override fun tryOpenDeepLink(deepLink: String): Boolean =
+        LoggingExceptionHandler.runCatching(
+            block = {
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(deepLink)).apply {
+                    addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                }
+                if (intent.resolveActivity(context.packageManager) != null) {
+                    if (intent.resolveActivityInfo(
+                            context.packageManager,
+                            ALL_MATCHING_ACTIVITIES_FLAG
+                        )?.exported == false
+                    ) {
                         intent.`package` = context.packageName
                     }
                     context.startActivity(intent)
                     true
-                } else false
+                } else {
+                    false
+                }
+            },
+            defaultValue = {
+                false
             }
-        }
-        catch (e: Exception) {
-            mindboxLogE(e.message ?: "exception when trying to tryOpenDeepLink")
-            return false
-        }
-    }
+        )
 }
