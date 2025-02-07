c99e9fd5e237ab5a11e05d7a5df5dd8431ca0716
sozinov <sozinov@mindbox.cloud>
2025-02-07 11:27:28 +0300

    WMSDK-402: remove runBlocking in onTrackVisitReady callback

--- sdk/src/main/java/cloud/mindbox/mobile_sdk/Mindbox.kt
+++ sdk/src/main/java/cloud/mindbox/mobile_sdk/Mindbox.kt
@@ -85,6 +85,9 @@ object Mindbox : MindboxLog {
     internal var mindboxScope = createMindboxScope()
         private set
 
+    internal var eventScope = createScopeWithIODispatcher()
+        private set
+
     private val tokenCallbacks = ConcurrentHashMap<String, (String?) -> Unit>()
     private val deviceUuidCallbacks = ConcurrentHashMap<String, (String) -> Unit>()
 
@@ -601,7 +604,7 @@ object Mindbox : MindboxLog {
                             inAppMessageManager.onStopCurrentActivity(resumedActivity)
                         },
                         onTrackVisitReady = { source, requestUrl ->
-                            runBlocking(Dispatchers.IO) {
+                            eventScope.launch {
                                 sendTrackVisitEvent(
                                     MindboxDI.appModule.appContext,
                                     source,
@@ -711,6 +714,10 @@ object Mindbox : MindboxLog {
 
     private fun createMindboxScope() = CoroutineScope(
         Default + SupervisorJob() + coroutineExceptionHandler,
+    )
+
+    private fun createScopeWithIODispatcher() = CoroutineScope(
+        Dispatchers.IO + SupervisorJob() + coroutineExceptionHandler,
     )
 
     private fun selectPushServiceHandler(
