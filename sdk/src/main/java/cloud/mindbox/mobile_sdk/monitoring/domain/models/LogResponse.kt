7d1a89230673d678b5e440f86106a7bdd8cc28c5
sozinov <sozinov@mindbox.cloud>
2025-02-27 16:09:32 +0300

    WMSDK-414: update visibility modifiers in mobile_sdk, mobile_sdk/logger, mobile_sdk/utils, mobile_sdk/pushes

--- sdk/src/main/java/cloud/mindbox/mobile_sdk/monitoring/domain/models/LogResponse.kt
+++ sdk/src/main/java/cloud/mindbox/mobile_sdk/monitoring/domain/models/LogResponse.kt
@@ -2,7 +2,7 @@ package cloud.mindbox.mobile_sdk.monitoring.domain.models
 
 import org.threeten.bp.ZonedDateTime
 
-internal class LogResponse(
+internal data class LogResponse(
     val zonedDateTime: ZonedDateTime,
     val log: String,
 )
