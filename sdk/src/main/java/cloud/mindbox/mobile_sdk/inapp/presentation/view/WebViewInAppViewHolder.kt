7c12c4a825f275a73b13d026a59658dbd1c8e7dc
Egor Kitselyuk <kitselyuk@mindbox.cloud>
2025-03-27 19:07:14 +0300

    Add fixes

--- sdk/src/main/java/cloud/mindbox/mobile_sdk/inapp/presentation/view/WebViewInAppViewHolder.kt
+++ sdk/src/main/java/cloud/mindbox/mobile_sdk/inapp/presentation/view/WebViewInAppViewHolder.kt
@@ -1,7 +1,6 @@
 package cloud.mindbox.mobile_sdk.inapp.presentation.view
 
 import android.annotation.SuppressLint
-import android.content.Intent
 import android.graphics.Color
 import android.view.KeyEvent
 import android.view.ViewGroup
@@ -17,11 +16,18 @@ import cloud.mindbox.mobile_sdk.inapp.presentation.MindboxView
 import cloud.mindbox.mobile_sdk.logger.mindboxLogD
 import cloud.mindbox.mobile_sdk.logger.mindboxLogI
 import cloud.mindbox.mobile_sdk.removeChildById
+import cloud.mindbox.mobile_sdk.repository.MindboxPreferences
 import com.android.volley.Request
 import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.VolleyError
+import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import kotlinx.coroutines.launch
+import org.json.JSONObject
+import java.nio.charset.Charset
+import java.util.Locale
 
 internal class WebViewInAppViewHolder(
     override val wrapper: InAppTypeWrapper<InAppType.WebView>,
@@ -45,84 +51,34 @@ internal class WebViewInAppViewHolder(
     }
 
     @SuppressLint("SetJavaScriptEnabled")
-    private fun createWebView(): WebView {
+    private fun createWebView(layer: Layer.WebViewLayer): WebView {
         mindboxLogI("WEBVIEW Create webview")
         return WebView(currentDialog.context).apply {
-            webViewClient = object : WebViewClient() {
-                override fun onReceivedError(
-                    view: WebView?,
-                    request: WebResourceRequest?,
-                    error: WebResourceError?
-                ) {
-                    mindboxLogI("WebView: $error")
-                    super.onReceivedError(view, request, error)
-                }
-
-                override fun onLoadResource(view: WebView?, url: String?) {
-                    super.onLoadResource(view, url)
-                    if (url == "https://personalization-web-staging.mindbox.ru/web/contacts/28553/") {
-                        mindboxLogI("onCompleted script. Close inapp")
-                        inAppCallback.onInAppDismissed(wrapper.inAppType.inAppId)
-                        hide()
-                    }
-                    mindboxLogI("onLoadResource. $url")
-                }
-
-                override fun shouldInterceptRequest(
-                    view: WebView?,
-                    request: WebResourceRequest?
-                ): WebResourceResponse? {
-                    mindboxLogI("shouldInterceptRequest request ${request?.url}}")
-                    val response = super.shouldInterceptRequest(view, request)
-                    return response
-                }
-
-                override fun shouldOverrideUrlLoading(
-                    view: WebView?,
-                    request: WebResourceRequest?
-                ): Boolean {
-                    mindboxLogD("shouldOverrideUrlLoading: ${request?.url}")
-                    if (request?.url.toString().contains("www.21vek.by")) {
-                        hide()
-                        val intent = Intent(Intent.ACTION_VIEW, request?.url)
-                        currentDialog.context.startActivity(intent)
-                        return true
-                    }
-                    return super.shouldOverrideUrlLoading(view, request)
-                }
-
-                override fun onPageFinished(view: WebView?, url: String?) {
-                    super.onPageFinished(view, url)
-                    mindboxLogD("WEBVIEW onPageFinished: ${view?.contentHeight} $url")
-                }
-
-                override fun shouldOverrideKeyEvent(view: WebView?, event: KeyEvent?): Boolean {
-                    return super.shouldOverrideKeyEvent(view, event)
-                }
-            }
+            webViewClient = InAppWebClient()
             addJavascriptInterface(
                 WebAppInterface { action, data ->
-                    this.post {
-                        mindboxLogI("WEBVIEW Action $action $data")
-                        when (action) {
-                            "collapse", "close" -> {
-                                if (data == "final") {
-                                    inAppCallback.onInAppClick(
-                                        wrapper.inAppType.inAppId,
-                                        "",
-                                        ""
-                                    )
-                                }
-                                hide()
-                            }
+                    handleWebViewAction(action, data, object : WebViewAction {
 
-                            "expand", "show" -> {
-                            }
-
-                            "go-item" -> {
-                            }
+                        override fun onInit() {
+                            webView?.visibility = ViewGroup.VISIBLE
                         }
-                    }
+
+                        override fun onCompleted() {
+                            inAppCallback.onInAppClick(
+                                wrapper.inAppType.inAppId,
+                                layer.actionUrl ?: "",
+                                ""
+                            )
+                            mindboxLogI("In-app dismissed by webview action")
+                            hide()
+                        }
+
+                        override fun onClose() {
+                            inAppCallback.onInAppDismissed(wrapper.inAppType.inAppId)
+                            mindboxLogI("In-app dismissed by webview action")
+                            hide()
+                        }
+                    })
                 },
                 "SDK"
             )
@@ -141,23 +97,25 @@ internal class WebViewInAppViewHolder(
         }
     }
 
-    @SuppressLint("SetJavaScriptEnabled", "JavascriptInterface")
-    fun addUrlSource(layer: Layer.WebViewLayer, inAppCallback: InAppCallback) {
+    @SuppressLint("SetJavaScriptEnabled")
+    fun addUrlSource(layer: Layer.WebViewLayer) {
         if (webView == null) {
             WebView.setWebContentsDebuggingEnabled(true)
-            webView = createWebView()
+            webView = createWebView(layer).also {
+                it.visibility = ViewGroup.INVISIBLE
+            }
             Mindbox.mindboxScope.launch {
                 val requestQueue: RequestQueue = Volley.newRequestQueue(currentDialog.context)
                 val stringRequest = StringRequest(
                     Request.Method.GET,
                     layer.contentUrl,
                     { response ->
-//                        val unEncodedHtml = currentDialog.context.assets
-//                            .open("webview.html")
-//                            .bufferedReader()
-//                            .use { it.readText() }
+                        val unEncodedHtml = currentDialog.context.assets
+                            .open("webview.html")
+                            .bufferedReader()
+                            .use { it.readText() }
 
-                        val unEncodedHtml = response
+                        //  val unEncodedHtml = response
 
                         webView?.loadDataWithBaseURL(
                             layer.baseUrl,
@@ -171,6 +129,7 @@ internal class WebViewInAppViewHolder(
                         hide()
                     }
                 )
+
                 requestQueue.add(stringRequest)
             }
         }
@@ -183,7 +142,7 @@ internal class WebViewInAppViewHolder(
         wrapper.inAppType.layers.forEach { layer ->
             when (layer) {
                 is Layer.WebViewLayer -> {
-                    addUrlSource(layer, inAppCallback)
+                    addUrlSource(layer)
                 }
 
                 else -> {
@@ -203,5 +162,114 @@ internal class WebViewInAppViewHolder(
     override fun hide() {
         currentDialog.removeView(webView)
         super.hide()
+    }
+
+    private interface WebViewAction {
+        fun onInit()
+        fun onCompleted()
+        fun onClose()
+    }
+
+    private fun WebView.handleWebViewAction(action: String, data: String, actions: WebViewAction) {
+        this.post {
+            mindboxLogI("handleWebViewAction: Action $action with $data")
+            when (action) {
+                "collapse", "close" -> {
+                    if (data == "final") {
+                        actions.onCompleted()
+                    }
+                    actions.onClose()
+                }
+
+                "init" -> {
+                    if (data == "quiz") {
+                        actions.onInit()
+                    } else {
+                        actions.onClose()
+                    }
+                }
+
+                "expand", "show", "go-item" -> {
+                }
+            }
+        }
+    }
+
+    internal class InAppWebClient() : WebViewClient() {
+        override fun onReceivedError(
+            view: WebView?,
+            request: WebResourceRequest?,
+            error: WebResourceError?
+        ) {
+            mindboxLogI("onReceivedError: $error")
+            super.onReceivedError(view, request, error)
+        }
+
+        override fun onLoadResource(view: WebView?, url: String?) {
+            mindboxLogI("onLoadResource: $url")
+            super.onLoadResource(view, url)
+        }
+
+        override fun shouldInterceptRequest(
+            view: WebView?,
+            request: WebResourceRequest?
+        ): WebResourceResponse? {
+
+            if (request?.url?.toString()?.lowercase(Locale.getDefault())?.endsWith("/favicon.ico") == true) {
+                val inputStream = "".byteInputStream(Charset.defaultCharset())
+                return WebResourceResponse("text", "UTF-8", inputStream)
+            }
+            mindboxLogI("shouldInterceptRequest request ${request?.url}}")
+
+            return super.shouldInterceptRequest(view, request)
+        }
+
+        override fun shouldOverrideUrlLoading(
+            view: WebView?,
+            request: WebResourceRequest?
+        ): Boolean {
+            mindboxLogD("shouldOverrideUrlLoading: ${request?.url}")
+
+            return super.shouldOverrideUrlLoading(view, request)
+        }
+
+        override fun onPageFinished(view: WebView?, url: String?) {
+            mindboxLogD("onPageFinished: $url")
+            super.onPageFinished(view, url)
+        }
+
+        override fun shouldOverrideKeyEvent(view: WebView?, event: KeyEvent?): Boolean {
+            mindboxLogD("shouldOverrideKeyEvent: $event")
+            return super.shouldOverrideKeyEvent(view, event)
+        }
+    }
+
+    class QuizzesRequest(
+        private val onSuccess: (JSONObject?) -> Unit
+    ) : JsonObjectRequest(
+        Method.POST,
+        "https://quizzes-staging.mindbox.ru/api/v0/init",
+        JSONObject(),
+        object : Response.Listener<JSONObject> {
+            override fun onResponse(response: JSONObject?) {
+                mindboxLogD("Response: $response")
+                onSuccess(response)
+            }
+        },
+        object : Response.ErrorListener {
+            override fun onErrorResponse(error: VolleyError?) {
+                mindboxLogD("Error: $error")
+                onSuccess(null)
+            }
+        }
+    ) {
+        override fun getHeaders(): MutableMap<String, String> {
+            return mutableMapOf(
+                "accept-language" to "en-US,en;q=0.9",
+                "x-client-id" to "Test-staging.mobile-sdk-test-staging.mindbox.ru",
+                "x-location" to "https://test-site-staging.mindbox.ru/mobile-sdk-test.html",
+                "x-user-uid" to MindboxPreferences.deviceUuid,
+            )
+        }
     }
 }
