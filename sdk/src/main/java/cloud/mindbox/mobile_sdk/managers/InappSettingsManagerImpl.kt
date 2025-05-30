5b323fa170042340198f12d6cd246d7da9c37992
sozinov <sozinov@mindbox.cloud>
2025-05-30 12:42:27 +0300

    WMSDK-469: add session limit checker for in-app messages

--- sdk/src/main/java/cloud/mindbox/mobile_sdk/managers/InappSettingsManagerImpl.kt
+++ sdk/src/main/java/cloud/mindbox/mobile_sdk/managers/InappSettingsManagerImpl.kt
@@ -3,7 +3,6 @@ package cloud.mindbox.mobile_sdk.managers
 import cloud.mindbox.mobile_sdk.Mindbox.logI
 import cloud.mindbox.mobile_sdk.inapp.data.managers.SessionStorageManager
 import cloud.mindbox.mobile_sdk.inapp.domain.models.InAppShowLimitsSettings
-import cloud.mindbox.mobile_sdk.models.Milliseconds
 import cloud.mindbox.mobile_sdk.models.operation.response.InAppConfigResponse
 
 internal class InappSettingsManagerImpl(val sessionStorageManager: SessionStorageManager) : InappSettingsManager {
@@ -16,9 +15,9 @@ internal class InappSettingsManagerImpl(val sessionStorageManager: SessionStorag
         """.trimIndent())
 
         sessionStorageManager.inAppShowLimitsSettings = InAppShowLimitsSettings(
-            maxInappsPerSession = config.settings?.inapp?.maxInappsPerSession ?: 0,
-            maxInappsPerDay = config.settings?.inapp?.maxInappsPerDay ?: 0,
-            minIntervalBetweenShows = config.settings?.inapp?.minIntervalBetweenShows ?: Milliseconds(0)
+            maxInappsPerSession = config.settings?.inapp?.maxInappsPerSession,
+            maxInappsPerDay = config.settings?.inapp?.maxInappsPerDay,
+            minIntervalBetweenShows = config.settings?.inapp?.minIntervalBetweenShows
         )
     }
 }
