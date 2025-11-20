a58e9c727aea414b2e3a63143207d64227491a04
sozinov <sozinov@mindbox.cloud>
2025-11-20 17:23:18 +0300

    fix flaky tests

--- sdk/src/test/java/cloud/mindbox/mobile_sdk/inapp/presentation/InAppMessageManagerTest.kt
+++ sdk/src/test/java/cloud/mindbox/mobile_sdk/inapp/presentation/InAppMessageManagerTest.kt
@@ -52,7 +52,7 @@ internal class InAppMessageManagerTest {
 
     private val inAppMessageDelayedManager = mockk<InAppMessageDelayedManager>()
 
-    private val testDispatcher = StandardTestDispatcher()
+    private val testDispatcher = UnconfinedTestDispatcher()
 
     /**
      * sets a thread to be used as main dispatcher for running on JVM
@@ -269,8 +269,6 @@ internal class InAppMessageManagerTest {
         }
 
         inAppMessageManager.listenEventAndInApp()
-        testScheduler.advanceTimeBy(200)
-        advanceUntilIdle()
 
         verify(exactly = 1) {
             MindboxLoggerImpl.e(Mindbox, "Mindbox caught unhandled error", exception)
