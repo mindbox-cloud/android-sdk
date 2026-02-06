8d9a627d9bb1f0f710cbb275562fe3062d113cf5
Sergey Sozinov <103035673+sergeysozinov@users.noreply.github.com>
2026-02-06 10:12:07 +0300

    MOBILEWEBVIEW-31: change default logic (#673)

--- sdk/src/main/java/cloud/mindbox/mobile_sdk/inapp/data/dto/deserializers/FeatureTogglesDtoBlankDeserializer.kt
+++ sdk/src/main/java/cloud/mindbox/mobile_sdk/inapp/data/dto/deserializers/FeatureTogglesDtoBlankDeserializer.kt
@@ -1,5 +1,6 @@
 package cloud.mindbox.mobile_sdk.inapp.data.dto.deserializers
 
+import cloud.mindbox.mobile_sdk.logger.mindboxLogW
 import cloud.mindbox.mobile_sdk.models.operation.response.SettingsDtoBlank
 import com.google.gson.JsonDeserializationContext
 import com.google.gson.JsonDeserializer
@@ -16,13 +17,18 @@ internal class FeatureTogglesDtoBlankDeserializer : JsonDeserializer<FeatureTogg
     ): FeatureTogglesDtoBlank {
         val jsonObject = json.asJsonObject
         val result = mutableMapOf<String, Boolean?>()
-
         jsonObject.entrySet().forEach { (key, value) ->
-            result[key] = value?.takeIf { it.isJsonPrimitive && it.asJsonPrimitive.isBoolean }
-                ?.asJsonPrimitive
-                ?.asBoolean
-        }
+            val booleanValue = when {
+                value?.isJsonPrimitive == true && value.asJsonPrimitive.isBoolean ->
+                    value.asJsonPrimitive.asBoolean
+                else -> {
+                    mindboxLogW("Feature toggle value is not boolean. key=$key, value=$value")
+                    null
+                }
+            }
 
+            result[key] = booleanValue
+        }
         return FeatureTogglesDtoBlank(toggles = result)
     }
 }
